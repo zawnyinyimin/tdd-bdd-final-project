f13f066fe6c7372d8ac1020601afedbfe37a35cd
zawnyinyimin <zawnyinyimin0@gmail.com>
2025-06-04 23:55:24 -0400

    tests/test_models.py FIND BY CATEGORY test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -163,4 +163,15 @@ class TestProductModel(unittest.TestCase):
         found = Product.find_by_name(name)
         self.assertEqual(found.count(), count)
         for product in found:
-            self.assertEqual(product.name, name)     def test_find_by_name(self):
+            self.assertEqual(product.name, name)
+      def test_find_by_category(self):
+        """It should Find Products by Category"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        category = products[0].category
+        count = len([product for product in products if product.category == category])
+        found = Product.find_by_category(category)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.category, category)     def test_find_by_name(self):
