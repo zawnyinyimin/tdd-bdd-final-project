2176d564a5928377af7e496a68d8cdd5dc5ef317
zawnyinyimin <zawnyinyimin0@gmail.com>
2025-06-05 00:56:29 -0400

    tests/test_routes.py read test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,6 +166,14 @@ class TestProductRoutes(TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
 
     ######################################################################
     # Utility functions
