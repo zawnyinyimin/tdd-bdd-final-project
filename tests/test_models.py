e6c4a1702828252c4eeb643f7149188c36222cee
zawnyinyimin <zawnyinyimin0@gmail.com>
2025-06-04 22:20:58 -0400

    tests/test_models.py find by name test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -153,3 +153,14 @@ class TestProductModel(unittest.TestCase):
         # See if we get back 5 products
         products = Product.all()
         self.assertEqual(len(products), 5)
+    def test_find_by_name(self):
+       """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)     def test_find_by_name(self):
