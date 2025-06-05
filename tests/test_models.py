007e838ffed3e77ecf72674db9007000a9e23073
zawnyinyimin <zawnyinyimin0@gmail.com>
2025-06-04 22:18:02 -0400

    tests/test_models.py list all test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -142,3 +142,14 @@ class TestProductModel(unittest.TestCase):
         # delete the product and make sure it isn't in the database
         product.delete()
         self.assertEqual(len(Product.all()), 0)
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        # Create 5 Products
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # See if we get back 5 products
+        products = Product.all()
+        self.assertEqual(len(products), 5)
